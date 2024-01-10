77ef08a20fbc307cba6d6b7c98d3c302583db4c4
yygit98 <85646668+yygit98@users.noreply.github.com>
2024-01-09 20:30:33 -0500

    Update app.js

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
-function callbackName(response) {
+function liveViews(response) {
     document.getElementById('visits').innerText = response.value;
 }
+// The function is called in the CDN on HTML.
+
